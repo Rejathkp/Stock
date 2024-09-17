141c5150db7ebf5512f2bc0442ade66d8c1f106f
Rejathkp <rejath.kallenparari@gmail.com>
2024-09-17 15:53:57 +0530

    subproduct commit

--- backend/Models/subProductModel.js
+++ backend/Models/subProductModel.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const subProductSchema = new mongoose.Schema({
-    code: { type:Number, required:true },
-    name: { type:String, required:true },
-    quantity: { type:Number, required:true },
-    price: { type:Number, required:true },
-    description: { type:String, required:true },
-});
-const subProductModel = mongoose.model.subProduct || mongoose.model("subProduct",subProductSchema)
+    subProductCode: { type:Number, required:true, unique:true },
+    subProductName: { type:String, required:true },
+    subProductMinimumQuantity: { type:Number, required:true },
+    subProductPrice: { type:Number, required:true },
+    subProductDescription: { type:String, required:true },
+},
+{ timestamps: true }
+);
+const subProductModel = mongoose.model("subProduct",subProductSchema)
 
 export default subProductModel;
